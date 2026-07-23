b00353ec1195c31252a331629309711d9cefb082
neonek26 <step.hofmann@seznam.cz>
2026-07-23 13:36:27 +0200

    Initial commit - Laravel setup s migracema

---
+++ app/Http/Controllers/ScreeningController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Screening;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ScreeningController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Screening $screening)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Screening $screening)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Screening $screening)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Screening $screening)
+    {
+        //
+    }
+}
