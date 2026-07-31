5440c21cb0076c12d4dbbba7a3fd7d6dc5f794e6
neonek26 <step.hofmann@seznam.cz>
2026-07-31 21:41:19 +0200

    Models upraveny

--- app/Models/Seat.php
+++ app/Models/Seat.php
@@ -3,8 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Seat extends Model
 {
-    //
+    protected $fillable = [
+        'hall_id',
+        'row',
+        'number',
+    ];
+
+    public function hall(): BelongsTo
+    {
+        return $this->belongsTo(Hall::class);
+    }
+
+    public function reservations(): HasMany
+    {
+        return $this->hasMany(Reservation::class);
+    }
 }
