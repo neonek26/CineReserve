9dc1e4aca80c98df3c808ad3da995f426ff49f47
neonek26 <step.hofmann@seznam.cz>
2026-09-17 20:15:19 +0200

    uprava hlavicky

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,8 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'CineReserve') }}</title>
 
+        <link rel="icon" type="image/svg+xml" href="data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 32 32'><rect width='32' height='32' rx='8' fill='%234f46e5'/><path d='M10 8v16M22 8v16M6 12h4m12 0h4M6 16h20M6 20h4m12 0h4M7 24h18a1 1 0 001-1V9a1 1 0 00-1-1H7a1 1 0 00-1 1v14a1 1 0 001 1z' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' fill='none'/></svg>">
+        
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
